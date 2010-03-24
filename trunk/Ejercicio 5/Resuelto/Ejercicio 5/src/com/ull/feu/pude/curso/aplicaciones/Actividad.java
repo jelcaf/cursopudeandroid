c9e69dec1798aafca51664c84eab3c8b8b851352
jelcaf <jelcaf@b3838845-36b6-c2c3-9f22-6352c5db9248>
2010-03-24 12:45:40 +0000

    Ejercicio 5: Sigue avanzando (no está terminado)

--- trunk/Ejercicio 5/Resuelto/Ejercicio 5/src/com/ull/feu/pude/curso/aplicaciones/Actividad.java
+++ trunk/Ejercicio 5/Resuelto/Ejercicio 5/src/com/ull/feu/pude/curso/aplicaciones/Actividad.java
@@ -6,12 +6,20 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class Actividad extends ListActivity {
+	
+	private static final int MENU_ADD_OR_DEL = Menu.FIRST;
+	private static final int MENU_ADD = Menu.FIRST + 1;
+	private static final int MENU_DEL = Menu.FIRST + 2;
 	
 	ArrayList<String> mArray = new ArrayList<String>(); 
 	
@@ -38,4 +46,31 @@ public class Actividad extends ListActivity {
                 android.R.layout.simple_list_item_1, mArray));
     }    
     
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	SubMenu sub = menu.addSubMenu (SubMenu.NONE, MENU_ADD_OR_DEL, SubMenu.NONE, "Opciones")
+    		.setIcon(android.R.drawable.ic_menu_more);
+    	
+    	sub.add(SubMenu.NONE, MENU_ADD, SubMenu.NONE, "Añadir elemento");
+    	sub.add(SubMenu.NONE, MENU_DEL, SubMenu.NONE, "Eliminar elemento");
+    	
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+    	switch (item.getItemId()) {
+    		case MENU_ADD:
+    			Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+    			break;
+    
+    		case MENU_DEL:
+    			Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+    			mArray.remove(mArray.size()-1);
+    	        setListAdapter(new ArrayAdapter<String>(this,
+    	                android.R.layout.simple_list_item_1, mArray));
+    			break;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
 }
