c8073bbeb91f75c7d139b83cd1ca080143a1537c
jelcaf <jelcaf@b3838845-36b6-c2c3-9f22-6352c5db9248>
2010-03-25 11:59:06 +0000

    Ejercicio 5 Resuelto

--- Ejercicio 5/Resuelto/Ejercicio 5/gen/com/ull/feu/pude/curso/aplicaciones/R.java
+++ Ejercicio 5/Resuelto/Ejercicio 5/gen/com/ull/feu/pude/curso/aplicaciones/R.java
@@ -13,8 +13,13 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int data_view=0x7f050000;
+        public static final int dato_edit=0x7f050001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int dialog_text_entry=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
