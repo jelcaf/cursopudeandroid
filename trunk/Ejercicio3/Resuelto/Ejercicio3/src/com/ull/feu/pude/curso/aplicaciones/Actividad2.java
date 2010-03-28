22c01abc16c3a31a600585fa8ac4b7dde4770447
jelcaf <jelcaf@b3838845-36b6-c2c3-9f22-6352c5db9248>
2010-03-28 11:33:07 +0000

    Ejercicio 3 - Hecho bien en trunk

--- trunk/Ejercicio3/Resuelto/Ejercicio3/src/com/ull/feu/pude/curso/aplicaciones/Actividad2.java
+++ trunk/Ejercicio3/Resuelto/Ejercicio3/src/com/ull/feu/pude/curso/aplicaciones/Actividad2.java
@@ -1,7 +1,10 @@
 package com.ull.feu.pude.curso.aplicaciones;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Actividad2 extends Activity {
@@ -19,5 +22,23 @@ public class Actividad2 extends Activity {
 
         TextView tV = (TextView) findViewById(R.id.TextView01);
         tV.setText(data);
+        
+        Button boton = (Button) findViewById (R.id.Button01);
+        boton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+               finish(); // código de la función
+            }
+        });
+
+        boton = (Button) findViewById (R.id.Button02);
+        boton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent result = new Intent();
+                result.putExtra("CLOSE", true);
+                setResult(RESULT_OK, result);
+                finish();
+                // código de la función
+            }
+        });
     }
 }
