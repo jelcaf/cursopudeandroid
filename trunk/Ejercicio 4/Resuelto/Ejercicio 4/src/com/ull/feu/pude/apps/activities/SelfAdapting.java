bd824ae359641dccd39bc522ab71f8004bedc05a
goldrunner192287 <goldrunner192287@b3838845-36b6-c2c3-9f22-6352c5db9248>
2010-03-30 17:01:31 +0000

    Committing solved assignment #4.

---
+++ trunk/Ejercicio 4/Resuelto/Ejercicio 4/src/com/ull/feu/pude/apps/activities/SelfAdapting.java
@@ -0,0 +1,16 @@
+package com.ull.feu.pude.apps.activities;
+
+import com.ull.feu.pude.apps.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelfAdapting extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //setContentView(R.layout.adaptive_main);
+    }
+}
