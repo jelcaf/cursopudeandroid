ab73a7cbaa2fbd34379555986b141067a1e70deb
goldrunner192287@gmail.com <goldrunner192287@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2010-11-24 01:46:56 +0000

    Uploading a new exercise.

---
+++ Ejercicios Básicos/Ejercicio Basico A_Vistas y Layouts/Resuelto/src/com/ull/feu/pude/apps/activities/ViewsAndLayouts.java
@@ -0,0 +1,102 @@
+package com.ull.feu.pude.apps.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.ull.feu.pude.apps.R;
+
+public class ViewsAndLayouts extends Activity {
+	
+	private CheckBox enableViews;
+	private TextView eventsTextView;
+	private LinearLayout myViewsLayout;
+	private RadioButton myRadioButton;
+	private int clickTimes;
+	private ToggleButton toggleVisibility;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // llamamos a nuestro método de inicialización
+        initConfig();
+    }
+    
+    private void initConfig() {
+    	setContentView(R.layout.main);
+    	enableViews = (CheckBox) findViewById(R.id.enableViews);
+    	eventsTextView = (TextView) findViewById(R.id.eventsTextView);
+    	myViewsLayout = (LinearLayout) findViewById(R.id.myViewsLayout);
+    	myRadioButton = (RadioButton) findViewById(R.id.myRadioButton);
+    	toggleVisibility = (ToggleButton) findViewById(R.id.toggleVisibility);
+    	
+    	/*
+    	 *  Asignamos un comportamiento al checkbox, de forma que cuando cambie se llame al 
+    	 *  método que tenemos dentro. Si el CheckBox está marcado (isChecked), ponemos el 
+    	 *  LinearLayout como visible, y sino hacemos que desaparezca.
+    	*/
+    	
+    	enableViews.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					myViewsLayout.setVisibility(View.VISIBLE);
+				}
+				else {
+					myViewsLayout.setVisibility(View.GONE);
+				}
+			}
+		});
+    	/*
+    	 * Con el RadioButton, lo que queremos es "escuchar" a cuando cambia de estado. Debido a sus 
+    	 * características, cuando un RadioButton es marcado, no puede volver a ser desmarcado, a menos 
+    	 * que lo forcemos nosotros.
+    	 */
+    	/*
+    	myRadioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				eventsTextView.setText("myRadioButton ha cambiado de estado.");				
+			}
+		});
+    	*/
+    	clickTimes = 0;
+    	myRadioButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				clickTimes++;
+				eventsTextView.setText("Clicked " + clickTimes + " times.");
+			}
+		});
+    	// Opcional
+    	/*
+    	myRadioButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (myRadioButton.isChecked())
+					myRadioButton.setChecked(false);
+				else
+					myRadioButton.setChecked(true);
+			}
+		});
+		*/
+    	toggleVisibility.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (!enableViews.isChecked()) {
+					if (isChecked)
+						myViewsLayout.setVisibility(View.INVISIBLE);
+					else
+						myViewsLayout.setVisibility(View.GONE);
+				}
+			}
+		});
+    }
+}
