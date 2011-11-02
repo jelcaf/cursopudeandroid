cb977f69109e107e838d59e8fc560061bb5954bb
goldrunner192287 <goldrunner192287@b3838845-36b6-c2c3-9f22-6352c5db9248>
2011-11-02 18:59:47 +0000

    Actualizado a Android 2.2, cambiados todos los "fill_parent" por "match_parent".

--- trunk/Ejercicios Básicos/Ejercicio Basico E_Interfaces Auto-Adaptativas/Resuelto/Ejercicio Basico E_Interfaces Auto-Adaptativas/src/com/ull/feu/pude/apps/activities/SelfAdapting.java
+++ trunk/Ejercicios Básicos/Ejercicio Basico E_Interfaces Auto-Adaptativas/Resuelto/Ejercicio Basico E_Interfaces Auto-Adaptativas/src/com/ull/feu/pude/apps/activities/SelfAdapting.java
@@ -10,7 +10,7 @@ public class SelfAdapting extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        //setContentView(R.layout.adaptive_main);
+        //setContentView(R.layout.main);
+        setContentView(R.layout.adaptive_main);
     }
 }
