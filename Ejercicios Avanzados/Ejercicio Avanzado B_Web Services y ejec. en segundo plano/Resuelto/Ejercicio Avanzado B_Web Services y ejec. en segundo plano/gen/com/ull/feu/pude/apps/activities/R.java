feb803e21517dde38155faf97fb7c4aea2100600
goldrunner192287 <goldrunner192287@b3838845-36b6-c2c3-9f22-6352c5db9248>
2011-11-05 17:31:03 +0000

    Actualizado.

--- Ejercicios Avanzados/Ejercicio Avanzado B_Web Services y ejec. en segundo plano/Resuelto/Ejercicio Avanzado B_Web Services y ejec. en segundo plano/gen/com/ull/feu/pude/apps/activities/R.java
+++ Ejercicios Avanzados/Ejercicio Avanzado B_Web Services y ejec. en segundo plano/Resuelto/Ejercicio Avanzado B_Web Services y ejec. en segundo plano/gen/com/ull/feu/pude/apps/activities/R.java
@@ -28,12 +28,12 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int defaultLatitude=0x7f040003;
-        public static final int defaultLongitude=0x7f040004;
+        public static final int defaultLongitude=0x7f040005;
         public static final int error=0x7f040008;
         public static final int getAddress=0x7f040007;
         public static final int hello=0x7f040000;
         public static final int latitude=0x7f040002;
-        public static final int longitude=0x7f040005;
+        public static final int longitude=0x7f040004;
         public static final int result=0x7f040006;
     }
 }
