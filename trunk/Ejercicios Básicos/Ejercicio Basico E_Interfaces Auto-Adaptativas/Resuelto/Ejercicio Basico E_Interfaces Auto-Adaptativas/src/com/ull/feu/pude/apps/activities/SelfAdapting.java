aa112534ea60abd74adc80114faf9c4fe710140f
goldrunner192287@gmail.com <goldrunner192287@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2012-12-01 18:10:22 +0000

    December 2012 - Header and version (from Froyo to Gingerbread) changes.

--- trunk/Ejercicios Básicos/Ejercicio Basico E_Interfaces Auto-Adaptativas/Resuelto/Ejercicio Basico E_Interfaces Auto-Adaptativas/src/com/ull/feu/pude/apps/activities/SelfAdapting.java
+++ trunk/Ejercicios Básicos/Ejercicio Basico E_Interfaces Auto-Adaptativas/Resuelto/Ejercicio Basico E_Interfaces Auto-Adaptativas/src/com/ull/feu/pude/apps/activities/SelfAdapting.java
@@ -5,6 +5,18 @@ import com.ull.feu.pude.apps.R;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * 
+ * Sencillo ejercicio en el que se intenta que los alumnos juegan y descubran c—mo funciona el RelativeLayout para que puedan 
+ * construir interfaces capaces de adaptarse por s’ solas a los distintos tipos de dispositivos con sus espec’ficos formatos 
+ * de pantalla. El XML "main" es est‡tico, utilizando un LinearLayout, mientras que el "adaptive_main" es el correcto, 
+ * basado en un RelativeLayout.
+ * 
+ * @author Dinesh Harjani (Twitter: @dinesharjani G+: +Dinesh Harjani E-mail:goldrunner18725@gmail.com)
+ * Hashtag: #droidissues
+ *
+ */
+
 public class SelfAdapting extends Activity {
     /** Called when the activity is first created. */
     @Override
