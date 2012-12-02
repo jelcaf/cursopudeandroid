3f5408b49fe0481ad7c6fc4b167c8ffd96938e80
goldrunner192287@gmail.com <goldrunner192287@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2012-12-02 18:49:09 +0000

    December 2012 refactoring.

--- trunk/Ejercicios Intermedios/Ejercicio Intermedio C_GPS/Resuelto/Ejercicio Intermedio C_GPS/gen/com/ull/feu/pude/apps/R.java
+++ trunk/Ejercicios Intermedios/Ejercicio Intermedio C_GPS/Resuelto/Ejercicio Intermedio C_GPS/gen/com/ull/feu/pude/apps/R.java
@@ -14,12 +14,14 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int latitudeTextView=0x7f050001;
-        public static final int longitudeTextView=0x7f050000;
-        public static final int toggleGPS=0x7f050002;
+        public static final int latitudeTextView=0x7f050002;
+        public static final int location_fragment=0x7f050000;
+        public static final int longitudeTextView=0x7f050001;
+        public static final int toggleGPS=0x7f050003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int main_activity=0x7f030000;
+        public static final int main_fragment=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
