eb94f05cc1b1c5a061051080b56220a56710a927
goldrunner192287@gmail.com <goldrunner192287@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2012-12-03 20:28:16 +0000

    Finished December 2012 revision of the rotation exercise.

--- Ejercicios Intermedios/Ejercicio Intermedio D_Rotacion/Resuelto/Ejercicio Intermedio D_Rotacion/src/com/ull/feu/pude/apps/activities/RotationalActivity.java
+++ Ejercicios Intermedios/Ejercicio Intermedio D_Rotacion/Resuelto/Ejercicio Intermedio D_Rotacion/src/com/ull/feu/pude/apps/activities/RotationalActivity.java
@@ -1,93 +1,36 @@
 package com.ull.feu.pude.apps.activities;
 
-import java.io.Serializable;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
 
 import com.ull.feu.pude.apps.R;
 
-public class RotationalActivity extends Activity {
+/**
+ * 
+ * Nuevamente, esta {@link Activity} es un veh’culo para poder utilizar el {@link Fragment} donde 
+ * verdaderamente se encuentra la implementaci—n de este ejercicio.
+ * 
+ * @author Dinesh Harjani (Twitter: @dinesharjani G+: +Dinesh Harjani E-mail:goldrunner18725@gmail.com)
+ * Hashtag: #droidissues
+ *
+ */
+
+public class RotationalActivity extends FragmentActivity {
+
 	private static final String LOG_TAG = RotationalActivity.class.getSimpleName();
-	
-	private Spinner source;
-	private EditText sourceText;
-	private Spinner destination;
-	private EditText destinationText;
-	
-	private String x;
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(LOG_TAG, "onCreate()");
-        initConfig(savedInstanceState);
+        initConfig();
     }
     
-    private void initConfig(Bundle savedInstanceState) {
-    	setContentView(R.layout.main);
-    	Log.i(LOG_TAG, "initConfig()");
-    	source = (Spinner) findViewById(R.id.sourceLanguage);
-    	sourceText = (EditText) findViewById(R.id.sourceText);
-    	destination = (Spinner) findViewById(R.id.destinationLanguage);
-    	destinationText = (EditText) findViewById(R.id.destinationText);
-    	
-    	if (savedInstanceState != null) {
-    		x = savedInstanceState.getString("x");
-    		sourceText.setText(x);
-    	}
-    	else {
-    		x = "Inicializado.";
-    	}
-    	Log.d(LOG_TAG, "Valor de x = " + x);
-    	
-    	// Array con las opciones de los Spinners
-    	String[] languages = new String[] {"Español", "Inglés", "Francés", "Alemán"};
-    	
-    	// Adaptador del Spinner lenguaje fuente
-    	ArrayAdapter<String> sourceAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, languages);
-    	sourceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    	source.setAdapter(sourceAdapter);
-    	
-    	/*
-    	// x guarda el contenido de el texto fuente
-    	sourceText.addTextChangedListener(new TextWatcher() {
-	    	public void afterTextChanged(Editable s) {
-	    		// después de que el texto cambie, asignamos x
-	    		//x = s.toString();
-	    		//Log.d(LOG_TAG, "Valor de x = " + x);
-	      	}
-	       	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-	       		// nada que hacer
-	       	}
-	       	public void onTextChanged(CharSequence s, int start, int before, int count) {
-	       		// nada que hacer
-	       	}
-    	});
-    	*/
-    	
-    	// Adaptador del Spinner lenguaje destino
-    	ArrayAdapter<String> destinationAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, languages);
-    	destinationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    	destination.setAdapter(destinationAdapter);
-    	// dejamos seleccionado el segundo elemento por defecto
-    	destination.setSelection(1);
+    private void initConfig() {
+    	setContentView(R.layout.main_activity);
     }
-    
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-    	x = sourceText.getText().toString();
-    	savedInstanceState.putString("x", x);
-    }
-    
     
 }
