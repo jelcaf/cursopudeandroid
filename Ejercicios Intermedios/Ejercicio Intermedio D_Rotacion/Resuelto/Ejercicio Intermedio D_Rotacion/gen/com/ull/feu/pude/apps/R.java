5ba69ad9716314a6641ed334a46bb3d4a3bd16e9
goldrunner192287@gmail.com <goldrunner192287@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2012-12-07 19:11:07 +0000

    Fixed a small error.

--- Ejercicios Intermedios/Ejercicio Intermedio D_Rotacion/Resuelto/Ejercicio Intermedio D_Rotacion/gen/com/ull/feu/pude/apps/R.java
+++ Ejercicios Intermedios/Ejercicio Intermedio D_Rotacion/Resuelto/Ejercicio Intermedio D_Rotacion/gen/com/ull/feu/pude/apps/R.java
@@ -14,14 +14,16 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int destinationLanguage=0x7f050003;
-        public static final int destinationText=0x7f050004;
-        public static final int screenCenter=0x7f050000;
-        public static final int sourceLanguage=0x7f050001;
-        public static final int sourceText=0x7f050002;
+        public static final int destinationLanguage=0x7f050004;
+        public static final int destinationText=0x7f050005;
+        public static final int rotational_fragment=0x7f050000;
+        public static final int screenCenter=0x7f050001;
+        public static final int sourceLanguage=0x7f050002;
+        public static final int sourceText=0x7f050003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int main_activity=0x7f030000;
+        public static final int main_fragment=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
