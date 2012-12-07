75a2c98a76ab7d540307156f305de6c11440620d
goldrunner192287@gmail.com <goldrunner192287@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2012-12-07 19:03:29 +0000

    Full code and comments have been updated to the December 2012 Edition.

--- Ejercicios Avanzados/Ejercicio Avanzado B_Web Services y ejec. en segundo plano/Resuelto/Ejercicio Avanzado B_Web Services y ejec. en segundo plano/src/com/ull/feu/pude/apps/activities/RequestActivity.java
+++ Ejercicios Avanzados/Ejercicio Avanzado B_Web Services y ejec. en segundo plano/Resuelto/Ejercicio Avanzado B_Web Services y ejec. en segundo plano/src/com/ull/feu/pude/apps/activities/RequestActivity.java
@@ -1,23 +1,17 @@
 package com.ull.feu.pude.apps.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.support.v4.app.FragmentActivity;
 
-import com.ull.feu.pude.apps.threads.RequestThread;
+/**
+ * Actividad simple que encapsula al Fragment responsable del comportamiento de esta aplicaci—n..
+ * 
+ * @author Dinesh Harjani (Twitter: @dinesharjani G+: +Dinesh Harjani E-mail:goldrunner18725@gmail.com)
+ * Hashtag: #droidissues
+ *
+ */
 
-public class RequestActivity extends Activity {
-	
-	public static final String __RESPONSE_KEY__ = "responseKey";
-	
-	private EditText latitudeET;
-	private EditText longitudeET;
-	private EditText resultET;
-	private Button requestButton;
+public class RequestActivity extends FragmentActivity {
 	
     /** Called when the activity is first created. */
     @Override
@@ -27,51 +21,7 @@ public class RequestActivity extends Activity {
     }
     
     private void initConfig() {
-    	setContentView(R.layout.main);
-    	
-    	// Obtener Views
-    	latitudeET = (EditText) findViewById(R.id.latitudeET);
-    	longitudeET = (EditText) findViewById(R.id.longitudeET);
-    	resultET = (EditText) findViewById(R.id.resultET);
-    	requestButton = (Button) findViewById(R.id.requestButton);
-    	
-    	// Asignar listeners
-    	requestButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				makeRequest();
-			}
-		});
+    	setContentView(R.layout.main_activity);	
     }
-    
-    private void makeRequest() {
-    	// desactivar el bot—n y los EditTexts para prevenir errores
-    	latitudeET.setEnabled(false);
-    	longitudeET.setEnabled(false);
-    	requestButton.setEnabled(false);
-    	
-    	
-    	// realizar la petici—n web
-    	RequestThread requestThread = new RequestThread(latitudeET.getText().toString(), 
-    			longitudeET.getText().toString(), myHandler);
-    	requestThread.run();
-    }
-    
-    Handler myHandler = new Handler() {
-    	
-		@Override
-		public void handleMessage(Message msg) {
-			if (msg.what == 0) {
-				// response
-				resultET.setText((String)msg.getData().get(RequestActivity.__RESPONSE_KEY__));
-			}
-			if (msg.what == 1) {
-				resultET.setText(getString(R.string.error));
-			}
-			// activamos todo
-	    	latitudeET.setEnabled(true);
-	    	longitudeET.setEnabled(true);
-	    	requestButton.setEnabled(true);
-		}
-    };
+
 }
