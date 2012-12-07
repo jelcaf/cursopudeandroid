7514b1e662db1000dbc910d89b6a5bcdc2b908d1
goldrunner192287@gmail.com <goldrunner192287@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2012-12-07 19:06:23 +0000

    Fixed a small error.

--- trunk/Ejercicios Avanzados/Ejercicio Avanzado A_Bases de Datos y conexion con ListView usando vistas personalizadas/Resuelto/Ejercicio Avanzado A_Bases de Datos y conexion con ListView usando vistas personalizadas/gen/com/ull/feu/pude/apps/R.java
+++ trunk/Ejercicios Avanzados/Ejercicio Avanzado A_Bases de Datos y conexion con ListView usando vistas personalizadas/Resuelto/Ejercicio Avanzado A_Bases de Datos y conexion con ListView usando vistas personalizadas/gen/com/ull/feu/pude/apps/R.java
@@ -14,7 +14,8 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int myList=0x7f050004;
+        public static final int listView_fragment=0x7f050004;
+        public static final int myList=0x7f050005;
         public static final int rowCheckBox=0x7f050000;
         public static final int rowLowerCenterLeft=0x7f050003;
         public static final int rowLowerLeft=0x7f050002;
@@ -22,7 +23,8 @@ public final class R {
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main_activity=0x7f030001;
+        public static final int main_fragment=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
