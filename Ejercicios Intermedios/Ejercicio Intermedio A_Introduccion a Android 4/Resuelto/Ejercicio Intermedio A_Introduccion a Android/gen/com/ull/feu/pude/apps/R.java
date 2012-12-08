9e4ec31598e512a8e4b26ac8aa6e74f1796c822b
goldrunner192287@gmail.com <goldrunner192287@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2012-12-08 18:31:25 +0000

    Pequeños cambios.

--- Ejercicios Intermedios/Ejercicio Intermedio A_Introduccion a Android 4/Resuelto/Ejercicio Intermedio A_Introduccion a Android/gen/com/ull/feu/pude/apps/R.java
+++ Ejercicios Intermedios/Ejercicio Intermedio A_Introduccion a Android 4/Resuelto/Ejercicio Intermedio A_Introduccion a Android/gen/com/ull/feu/pude/apps/R.java
@@ -41,19 +41,13 @@ public final class R {
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+        Tema base de la aplicación que depende de la API. Utilizamos el clásico negro sobre blanco.
     
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
  Tema holográfico para Android v4  
          */
         public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        /**  Tema de la Aplicación 
+ Todos los cambios independientes del nivel de API que queramos hacer van aquí. 
          */
         public static final int AppTheme=0x7f060001;
     }
