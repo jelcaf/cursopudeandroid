684893c48c037a75924eb5db1ae575408dce88fe
jelcaf@gmail.com <jelcaf@gmail.com@b3838845-36b6-c2c3-9f22-6352c5db9248>
2012-12-10 08:40:58 +0000

    Update to Android 2.3.3 - Ejercicio Básico F Inicial

--- trunk/Ejercicios Básicos/Ejercicio Basico F_Multiples Actividades/Inicial/Multiples Actividades/src/com/ull/feu/pude/curso/aplicaciones/Actividad1.java
+++ trunk/Ejercicios Básicos/Ejercicio Basico F_Multiples Actividades/Inicial/Multiples Actividades/src/com/ull/feu/pude/curso/aplicaciones/Actividad1.java
@@ -3,6 +3,12 @@ package com.ull.feu.pude.curso.aplicaciones;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * @author Jorge Carballo (Twitter: @jelcaf G+: +Jorge Carballo E-mail:jelcaf@gmail.com)
+ * @author Dinesh Harjani (Twitter: @dinesharjani G+: +Dinesh Harjani E-mail:goldrunner18725@gmail.com)
+ * Hashtag: #droidissues
+ *
+ */
 public class Actividad1 extends Activity {
     /** Called when the activity is first created. */
     @Override
